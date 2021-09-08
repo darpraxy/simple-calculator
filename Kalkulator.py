e1f9c2a725ac9598e4e7bbc1bf6546da6cb91030
darpraxy <90315929+darpraxy@users.noreply.github.com>
2021-09-08 16:30:35 +0700

    Add files via upload

---
+++ Kalkulator.py
@@ -0,0 +1,18 @@
+a = input('Ini adalah kalkulator, jadi kamu mau menghitung apa:\n' ) 
+b = input() 
+c = input()
+
+if b == '+':
+    print('hasilnya:')	
+    print(int(a) + int(c))
+if b == "-":
+    print('hasilnya:')
+    print(int(a) - int(c))
+if b == "x":
+    print('hasilnya:')
+    print(int(a) * int(c))
+if b == "÷":
+    print('hasilnya:')
+    print(int(a) / int(c))
+
+a = input('\nnanika youka? ')
